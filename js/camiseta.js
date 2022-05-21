5ea9416eb3e6a6938a832577ab31ab890c9196e0
EdgarRB <37355506+shadoblack@users.noreply.github.com>
2022-05-21 13:39:49 +0200

    constructores

--- js/camiseta.js
+++ js/camiseta.js
@@ -1,7 +1,14 @@
 //clase (molde del objeto)
 var Camiseta = /** @class */ (function () {
-    function Camiseta() {
+    //constructor(forma parte de funciones)
+    function Camiseta(color, modelo, marca, talla, precio) {
+        this.color = color;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.talla = talla;
+        this.precio = precio;
     }
+    ;
     //metodos (funciones o acciones que va a hacer el objeto)
     Camiseta.prototype.setColor = function (color) {
         this.color = color;
@@ -11,16 +18,17 @@ var Camiseta = /** @class */ (function () {
     };
     return Camiseta;
 }());
-var camiseta = new Camiseta();
-camiseta.setColor("rojo");
-camiseta.modelo = "manga larga";
-camiseta.marca = "Adidas";
-camiseta.talla = "L";
-camiseta.precio = 10;
-var playera = new Camiseta();
-playera.setColor("azul");
-playera.modelo = "manga corta";
-playera.marca = "Adidas";
-playera.talla = "XL";
-playera.precio = 5;
-console.log(camiseta, playera);
+;
+var camiseta = new Camiseta("rojo", "manga larga", "adidas", "L", 10);
+// camiseta.setColor("rojo");
+// camiseta.modelo = "manga larga";
+// camiseta.marca = "Adidas";
+// camiseta.talla = "L";
+// camiseta.precio = 10;
+// var playera = new Camiseta();
+// playera.setColor("azul");
+// playera.modelo = "manga corta";
+// playera.marca = "Adidas";
+// playera.talla = "XL";
+// playera.precio = 5;
+console.log(camiseta);
--- js/camiseta.ts
+++ js/camiseta.ts
@@ -1,40 +1,48 @@
 //clase (molde del objeto)
-class Camiseta{
-    //propiedades (caracteristicas del objeto)
-    private color: string;
-    public modelo: string;
-    public marca: string;
-    public talla: string;
-    public precio: number;
+class Camiseta {
+  //propiedades (caracteristicas del objeto)
+  private color: string;
+  public modelo: string;
+  public marca: string;
+  public talla: string;
+  public precio: number;
 
-    //metodos (funciones o acciones que va a hacer el objeto)
+   //constructor(forma parte de funciones)
 
-    public setColor(color){
-        this.color = color;
-    }
-    public getColor(){
-        return this.color
-    }
-}
+   constructor(color,modelo,marca,talla,precio,) {
+    this.color = color;
+    this.modelo = modelo;
+    this.marca = marca;
+    this.talla = talla;
+    this.precio = precio;
+  };
 
+  //metodos (funciones o acciones que va a hacer el objeto)
 
+  public setColor(color) {
+    this.color = color;
+  }
+  public getColor() {
+    return this.color;
+  }
 
+ 
+};
 
+var camiseta = new Camiseta("rojo","manga larga","adidas","L",10);
 
-var camiseta = new Camiseta();
+// camiseta.setColor("rojo");
+// camiseta.modelo = "manga larga";
+// camiseta.marca = "Adidas";
+// camiseta.talla = "L";
+// camiseta.precio = 10;
 
-camiseta.setColor("rojo");
-camiseta.modelo ="manga larga";
-camiseta.marca = "Adidas";
-camiseta.talla = "L";
-camiseta.precio = 10;
+// var playera = new Camiseta();
 
-var playera = new Camiseta();
+// playera.setColor("azul");
+// playera.modelo = "manga corta";
+// playera.marca = "Adidas";
+// playera.talla = "XL";
+// playera.precio = 5;
 
-playera.setColor("azul");
-playera.modelo ="manga corta";
-playera.marca = "Adidas";
-playera.talla = "XL";
-playera.precio = 5;
-
-console.log(camiseta,playera);
+console.log(camiseta);
